13e5cbdce4d40bdaa61202044c44740b2a8a58fc
KatoCode23 <Kalen23Imani@gmail.com>
2019-09-26 12:44:50 -0400

    who is older

--- exercises/js-conditionals/script.js
+++ exercises/js-conditionals/script.js
@@ -1,14 +1,42 @@
-function whoIsOlder(person1, person2) {
-  console.log('Sina is older than Tom');
-};
+console.log('I am here');
+
 
 var me = {
-  name: 'Sina',
-  age: 33
+  name: 'Kalen',
+  age: 37
 };
-var mySibling = {};
-var myFriend = {};
 
-whoIsOlder(me, mySibling);
-whoIsOlder(me, myFriend);
-whoIsOlder(mySibling, myFriend);
+var myBestMan = {
+  name: 'John',
+  age: 40
+};
+
+var myGroomsmen = {
+  name: 'George',
+  age: 38
+};
+
+console.log(me, myBestMan, myGroomsmen);
+
+function whoIsOlder(person1, person2) {
+  if (person1.age > person2.age) {
+    return person1.name + ' is older than ' + person2.name;
+  }
+
+  else if (person2.age > person1.age) {
+    return person2.name + ' is older than ' + person1.name;
+  } 
+};
+
+var ageComparison1 = whoIsOlder(myBestMan, myGroomsmen);
+console.log(ageComparison1);
+var ageComparison2 = whoIsOlder(me, myGroomsmen);
+console.log(ageComparison2);
+var ageComparison3 = whoIsOlder(me, myBestMan);
+console.log(ageComparison3); 
+var ageComparison4 = whoIsOlder(myGroomsmen, myBestMan);
+console.log(ageComparison4);
+var ageComparison5 = whoIsOlder(myGroomsmen, me);
+console.log(ageComparison5);
+var ageComparison6 = whoIsOlder(myBestMan, me);
+console.log(ageComparison6); 
