3f6d8ae91cc116187ecd808cc98a6cb3b4913286
KatoCode23 <Kalen23Imani@gmail.com>
2019-09-26 20:04:25 -0400

    test

--- exercises/js-conditionals/script.js
+++ exercises/js-conditionals/script.js
@@ -19,13 +19,20 @@ var myGroomsmen = {
 console.log(me, myBestMan, myGroomsmen);
 
 function whoIsOlder(person1, person2) {
+  var message = '';
+  var ageDiffernce;
+  
   if (person1.age > person2.age) {
-    return person1.name + ' is older than ' + person2.name;
+    ageDiffernce = person1.age - person2.age;
+    message = ( person1.name + ' is older than ' + person2.name + ' by ' + ageDiffernce + ' years.');
   }
 
-  else if (person2.age > person1.age) {
-    return person2.name + ' is older than ' + person1.name;
-  } 
+  if (person2.age > person1.age) {
+    ageDiffernce = person2.age - person1.age;
+    message = ( person2.name + ' is older than ' + person1.name  + ' by ' + ageDiffernce + ' years.' );
+  }
+  console.log(message);
+
 };
 
 var ageComparison1 = whoIsOlder(myBestMan, myGroomsmen);
